80df48c0d19597eb170cf09cb23a7f49f056c4ae
aleynakuvan <114821414+aleynakuvan@users.noreply.github.com>
2023-03-09 16:21:38 +0300

    Add files via upload

---
+++ User-migrations/Migrations/202212110744450_InitialCreate.cs
@@ -0,0 +1,14 @@
+﻿
+namespace User.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+           
+        }
+    }
+}
---
+++ User-migrations/Migrations/Configurations.cs
@@ -0,0 +1,24 @@
+﻿namespace Library.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Library.ORM.Context.Projectcontext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Library.ORM.Context.Projectcontext";
+        }
+
+        protected override void Seed(Library.ORM.Context.Projectcontext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
